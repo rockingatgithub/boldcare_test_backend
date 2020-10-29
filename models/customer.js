7428a07319bf426fd00c6b8774d1fcdc36e3f2de
rockingatgithub <contactwithsid@gmail.com>
2020-10-29 18:24:33 +0530

    models defined

---
+++ models/customer.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const customerSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Customer = mongoose.model("Product", customerSchema);
+module.exports = Customer;
---
+++ models/review.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Customer = require("./customer");
+const Product = require("./product");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: Product,
+    },
+    customerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: Customer,
+    },
+    stars: {
+      type: Number,
+      default: 5,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ["Accepted", "Rejected"],
+      default: "Accepted",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Review = mongoose.model("Product", reviewSchema);
+module.exports = Review;
